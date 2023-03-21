ed2fe81493a7d010457347ffd4175ef945177b75
Craig Ingram <cinple.sln@gmail.com>
2023-03-21 00:37:07 -0400

    Intial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# k8s-parallel-exec
+
+A Golang-based tool for executing commands in parallel on Kubernetes pods with specified labels, similar to GNU Parallel.
+
+## Features
+
+- Execute commands in parallel on multiple Kubernetes pods based on label selectors.
+- Specify the target container in the pod.
+- Aggregate results from all pods and display them in an easy-to-read format.
+
+## Requirements
+
+- Go 1.16 or higher
+- A valid kubeconfig file or in-cluster configuration
+- Access to a Kubernetes cluster
+
+## Installation
+
+1. Clone the repository:
+
+```sh
+git clone https://github.com/your-github-username/k8s-parallel-exec.git
+```
+
+2. Change into the project directory:
+```sh
+cd k8s-parallel-exec
+```
+
+3. Build the binary:
+```sh
+go build -o k8s-parallel-exec
+```
+
+4. Move the binary to a directory in your PATH (optional):
+```sh
+sudo mv k8s-parallel-exec /usr/local/bin/
+```
+
+## Usage
+```sh
+k8s-parallel-exec -kubeconfig /path/to/kubeconfig -c container-name -l label-selector command-to-execute
+```
+- `-kubeconfig`: Path to the kubeconfig file. If not provided, in-cluster configuration will be used.
+- `-c`: Container to execute the command against.
+- `-l`: Label selector to filter the pods.
+- `command-to-execute`: The command to run inside the specified container in each pod.
+
+
+Example
+```sh
+k8s-parallel-exec -kubeconfig ~/.kube/config -c cassandra -l app=cassandra nodetool status
+```
+This command would execute `nodetool status` on all the Cassandra containers in pods with the label "app=cassandra" in parallel, and then aggregate and display the results.
+
+## Credits
+The initial implementation for this project were provided by ChatGPT from OpenAI.
---
+++ main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"sync"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/remotecommand"
+)
+
+type ColorCode int
+
+const (
+	BrightRed ColorCode = iota + 91
+	BrightGreen
+	BrightYellow
+	BrightBlue
+	BrightMagenta
+	BrightCyan
+	BrightWhite
+)
+
+const (
+	divText = "----------------------------------------------------\n"
+)
+
+var (
+	divBarColor  = BrightYellow
+	podNameColor = BrightCyan
+)
+
+func main() {
+	kubeconfig := flag.String("kubeconfig", "", "Path to the kubeconfig file")
+	containerFlag := flag.String("c", "", "Container to execute the command against")
+	labelFlag := flag.String("l", "", "Label selector to filter pods")
+	flag.Parse()
+
+	if *containerFlag == "" {
+		fmt.Println("Error: container name must be specified with -c")
+		os.Exit(1)
+	}
+
+	if *labelFlag == "" {
+		fmt.Println("Error: label selector must be specified with -l")
+		os.Exit(1)
+	}
+
+	args := flag.Args()
+	if len(args) == 0 {
+		fmt.Println("Error: command to execute is required")
+		os.Exit(1)
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	pods, err := clientset.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{
+		LabelSelector: *labelFlag,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	var wg sync.WaitGroup
+	results := make(chan string, len(pods.Items))
+
+	for _, pod := range pods.Items {
+		wg.Add(1)
+		go func(p v1.Pod) {
+			defer wg.Done()
+			output, err := execCommand(config, clientset, p, *containerFlag, args)
+			if err != nil {
+				results <- fmt.Sprintf("Error executing command on pod %s: %v", p.Name, err)
+			} else {
+				results <- fmt.Sprintf("%sPod %s\n%s%s",
+					colorize(divBarColor, divText),
+					colorize(podNameColor, p.Name),
+					colorize(divBarColor, divText),
+					output)
+			}
+		}(pod)
+	}
+
+	wg.Wait()
+	close(results)
+
+	for result := range results {
+		fmt.Println(result)
+	}
+}
+
+func colorize(colorCode ColorCode, text string) string {
+	return fmt.Sprintf("\033[%dm%s\033[0m", colorCode, text)
+}
+
+func execCommand(config *rest.Config, clientset *kubernetes.Clientset, pod v1.Pod, container string, command []string) (string, error) {
+	req := clientset.CoreV1().RESTClient().Post().
+		Resource("pods").
+		Name(pod.Name).
+		Namespace(pod.Namespace).
+		SubResource("exec").
+		VersionedParams(&v1.PodExecOptions{
+			Container: container,
+			Command:   command,
+			Stdout:    true,
+			Stderr:    true,
+		}, scheme.ParameterCodec)
+
+	exec, err := remotecommand.NewSPDYExecutor(config, "POST", req.URL())
+	if err != nil {
+		return "", err
+	}
+
+	var stdout, stderr bytes.Buffer
+	err = exec.StreamWithContext(context.Background(), remotecommand.StreamOptions{
+		Stdout: bufio.NewWriter(&stdout),
+		Stderr: bufio.NewWriter(&stderr),
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	if stderr.Len() > 0 {
+		return stdout.String(), fmt.Errorf("stderr: %s", stderr.String())
+	}
+
+	return stdout.String(), nil
+}
